e7b05c5b82cd1372cdafd98bb36dfe545576a73b
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-11 23:10:00 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\");
+        sb.append("\n");
+        sb.append(" )  ( ')");
+        sb.append("\n");
+        sb.append("(  /  )");
+        sb.append("\n");
+        sb.append(" \\(__)|");
+        sb.append("\n");
+        System.out.println(sb);
+    }
+}
