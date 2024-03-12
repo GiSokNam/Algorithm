76325b41b986b0769f78336850a71e0d5785ef8c
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-12 22:32:08 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        int b = Integer.parseInt(br.readLine());
+        for (int i : a) {
+            if (i == b) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
