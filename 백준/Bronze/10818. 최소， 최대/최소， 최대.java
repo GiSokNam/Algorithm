65e20e2445a1885b4ea43c559e7ed4c6070508cc
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-13 22:53:23 +0900

    [Bronze III] Title: 최소, 최대, Time: 512 ms, Memory: 92444 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -4,7 +4,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] a = new int[n];
@@ -18,8 +17,6 @@ public class Main {
             if(min > a[i]) min = a[i];
             if(max < a[i]) max = a[i];
         }
-        bw.write(min + " "+ max);
-        bw.flush();
-        bw.close();
+        System.out.println(min + " " + max);
     }
 }
