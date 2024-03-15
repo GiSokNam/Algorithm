3388a9194e340a89b1bc94d8cccf13fe1b6b219f
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-15 18:20:06 +0900

    [Bronze II] Title: 나머지, Time: 132 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int[] arr = new int[10];
+        int a;
+        for (int i = 0; i < 10; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            arr[i] = a%42;
+        }
+        br.close();
+        int[] newArr = Arrays.stream(arr).distinct().toArray();
+        bw.write(newArr.length + "");
+        bw.flush();
+        bw.close();
+    }
+}
