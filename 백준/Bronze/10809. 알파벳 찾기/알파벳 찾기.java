d2e38eba287237b3b679d42b65ea5d2a023ba23b
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-17 17:33:11 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15940 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        br.close();
+        int[] arr = new int[26];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += -1;
+        }
+        int location = 0;
+        for (int i = 0; i < str.length(); i++) {
+            location = str.charAt(i) - 'a';
+            if (arr[location] == -1) {
+                arr[location] = i;
+            }
+        }
+
+        for (int i : arr) {
+            bw.write(i + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
