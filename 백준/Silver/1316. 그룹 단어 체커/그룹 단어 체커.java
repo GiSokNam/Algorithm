e2d72035d11e3304263ea522ea52aac820b7c657
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-19 19:57:11 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String str = "";
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            str = br.readLine();
+            if (check(str)) {
+                count++;
+            }
+        }
+        br.close();
+        System.out.print(count);
+
+    }
+
+    static boolean check(String str) {
+        boolean[] check = new boolean[26];
+        int pre = 0;
+        int now = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            now = str.charAt(i);
+            if (pre != now) {
+                if (!check[now - 97]) {
+                    check[now - 97] = true;
+                    pre = now;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
