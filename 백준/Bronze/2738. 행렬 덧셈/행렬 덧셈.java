7b6648a4cff27aa03b10c5762ae354ffb1ef1889
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-20 18:14:09 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 440 ms, Memory: 18776 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] A = new int[N][M];
+        int[][] B = new int[N][M];
+        int[][] C = new int[N][M];
+
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < N; j++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                for (int k = 0; k < M; k++) {
+                    if(i == 0) A[j][k] = Integer.parseInt(st.nextToken());
+                    if(i == 1) B[j][k] = Integer.parseInt(st.nextToken());
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
