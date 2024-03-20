a0138c1bc582f9b5e86470bbb27f802b7ff494c8
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-20 19:31:37 +0900

    [Bronze I] Title: 세로읽기, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,11 +1,9 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
         String[][] arr = new String[5][15];
 
