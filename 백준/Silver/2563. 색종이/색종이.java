b874f39795a42771666ed6dc6badd9f269626bca
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-21 18:33:18 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+        int[][] paper = new int[100][100];
+        int x;
+        int y;
+
+        for (int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    paper[j][k] = 1;
+                }
+            }
+        }
+        br.close();
+        int sum = 0;
+        for (int i = 0; i < paper.length; i++) {
+            for (int j = 0; j < paper.length; j++) {
+                if (paper[i][j] == 1) {
+                    sum += 1;
+                }
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
