7305358ef8264e8a63eb010c73468486ea3f79d4
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-27 19:03:04 +0900

    [Bronze III] Title: 세 막대, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int[] t = new int[] {a, b, c};
+        br.close();
+        Arrays.sort(t);
+        int sum;
+        if (t[2] >= t[1] + t[0]) {
+            sum = (t[1] + t[0] - 1) + t[1] + t[0] ;
+        } else {
+            sum = t[2] + t[1] + t[0];
+        }
+        bw.write(sum+"");
+        bw.flush();
+        bw.close();
+    }
+}
