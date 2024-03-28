e8f9e8758601987f4613f451e4cf190b127e0f22
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-03-28 14:48:50 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 128 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Long n = Long.parseLong(br.readLine());
+        Long time = n * n;
+        br.close();
+        bw.write(time + "");
+        bw.newLine();
+        bw.write(2 + "");
+        bw.flush();
+        bw.close();
+    }
+}
