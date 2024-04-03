cfb9076b5d3dd296ed43de2a3131b4cb3503ec15
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-04-03 22:03:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] desc = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            desc[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+
+        Arrays.sort(desc);
+
+        for (int i : desc) {
+            bw.write(i + "");
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+
+    }
+}
