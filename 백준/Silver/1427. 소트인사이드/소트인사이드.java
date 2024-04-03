aea1dc1ecfa47f6e7e71f27e4c020104f9b45400
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-04-03 22:52:45 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String n = br.readLine();
+        Long[] N = new Long[n.length()];
+        br.close();
+        for (int i = 0; i < n.length(); i++) {
+            N[i] = Long.parseLong(String.valueOf(n.charAt(i)));
+        }
+        Arrays.sort(N);
+        String temp ="";
+        for (int i = 0; i < N.length; i++) {
+            temp += String.valueOf(N[i]);
+        }
+        sb.append(temp);
+        bw.write(sb.reverse().toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
