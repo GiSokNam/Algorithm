b22f618ddcc59ec844408dfabd5bad29386edf0f
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-04-04 12:40:00 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -9,9 +9,11 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-	int c = Integer.parseInt(st.nextToken());
-	if (a == b && a == c) {
-            System.out.println(10000 + a * 1000);
+	    int c = Integer.parseInt(st.nextToken());
+        br.close();
+	
+	    if (a == b && a == c) {
+                System.out.println(10000 + a * 1000);
         } else if (a == b) {
             System.out.println(1000 + a * 100);
         } else if (a == c) {
@@ -21,5 +23,6 @@ public class Main {
         } else {
             System.out.println(Math.max(a, Math.max(b,c)) * 100);
         }
+         
     }
 }
