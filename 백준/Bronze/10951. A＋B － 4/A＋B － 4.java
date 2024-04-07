ef5ab6de7a68e57e1bd65f8f0da0b7e8b3560169
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-04-07 19:40:29 +0900

    [Bronze V] Title: A+B - 4, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -2,33 +2,25 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringTokenizer st;
+        int A;
+        int B;
+        String str;
 
-        try {
-            StringTokenizer st;
-            String str = "";
-
-            while ((str=br.readLine()) != null) {
-
-                st = new StringTokenizer(str);
-                if (st.hasMoreTokens()) {
-                    bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "");
-                    bw.newLine();
-                } else {
-                    break;
-                }
-            }
-            br.close();
-            bw.flush();
-            bw.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
+            if(!st.hasMoreTokens()) break;
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            bw.write((A+B)+"");
+            bw.newLine();
         }
-
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
