6bdcfbfd1948728788b2d83fe963f817bba82fec
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-05-01 12:41:51 +0900

    [Silver IV] Title: 스택 2, Time: 944 ms, Memory: 220276 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[] stack;
+    public static int size = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        stack = new int[N];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            switch (Integer.parseInt(st.nextToken())) {
+                case 1 :
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+                case 2 :
+                    bw.write(pop()+"");
+                    bw.newLine();
+                    break;
+                case 3 :
+                    bw.write(size() + "");
+                    bw.newLine();
+                    break;
+                case 4 :
+                    bw.write(empty() + "");
+                    bw.newLine();
+                    break;
+                case 5 :
+                    bw.write(top() + "");
+                    bw.newLine();
+                    break;
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void push(int i) {
+        stack[size] = i;
+        size++;
+    }
+
+    public static int pop() {
+        if (size == 0) {
+            return -1;
+        } else {
+            int i = stack[size - 1];
+            stack[size - 1] = 0;
+            size--;
+            return i;
+        }
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if (size == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int top() {
+        if (size == 0) {
+            return -1;
+        } else {
+            return stack[size - 1];
+        }
+    }
+}
