5f18568a47759e74a4fe95564a9c62ec6511f224
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-05-11 14:49:36 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 216 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        Deque<int[]> deque = new ArrayDeque<>();
+        int[] num = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < num.length; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < N; i++) {
+            deque.offer(new int[]{(i + 1), num[i]});
+        }
+
+        br.close();
+
+        while (!deque.isEmpty()) {
+            int idx = deque.peek()[0];
+            int number = deque.peek()[1];
+
+            if (idx == 1) {
+                bw.write(deque.remove()[0] + " ");
+
+                if (number > 0) {
+                    number -= 1;
+                    while (number-- > 0) {
+                        deque.offerLast(deque.pollFirst());
+                    }
+                } else {
+                    while (number++ < 0) {
+                        deque.offerFirst(deque.pollLast());
+                    }
+                }
+            } else {
+                bw.write(deque.remove()[0] + " ");
+                if (deque.size() == 0) {
+                    break;
+                }
+
+                if (number > 0) {
+                    number -= 1;
+                    while (number-- > 0) {
+                        deque.offerLast(deque.pollFirst());
+                    }
+                } else {
+                    while (number++ < 0) {
+                        deque.offerFirst(deque.pollLast());
+                    }
+                }
+            }
+
+        }
+        bw.flush();
+        bw.close();
+    }
+}
