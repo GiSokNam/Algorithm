1efc21b759b4898e04d2c12c22ac87d21acd397a
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-05-14 14:09:10 +0900

    [Bronze IV] Title: 녹색거탑, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        int cnt = (int) Math.pow(2, N);
+        
+        bw.write(cnt + "");
+        bw.flush();
+        bw.close();
+    }
+}
