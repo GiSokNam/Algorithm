bb7aee06318ae0921892fba17a07a0262a62fe40
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-05-27 20:51:08 +0900

    [Silver III] Title: 칸토어 집합, Time: 360 ms, Memory: 61640 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    static char[] lineArr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str;
+        int N;
+
+        while ((str = br.readLine()) != null) {
+            N = Integer.parseInt(str);
+
+            int len = (int) Math.pow(3, N);
+
+            lineArr = new char[len];
+            Arrays.fill(lineArr, '-');
+
+            divide(0, len);
+
+            for (int i = 0; i < lineArr.length; i++) {
+                bw.write(lineArr[i]+"");
+            }
+
+            bw.newLine();
+            bw.flush();
+        }
+        br.close();
+        bw.close();
+    }
+
+    static void divide(int start, int length) {
+        
+        if(length <= 1){
+            return;
+        }
+
+        int newlength = length/3;
+        for(int i = start+newlength; i < start+newlength*2; i++){
+            lineArr[i] = ' ';
+        }
+
+        divide(start, newlength);
+        divide(start+newlength*2, newlength);
+    }
+
+}
