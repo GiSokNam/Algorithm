78c4b97cf23a7da47048d7fc584d077079f65f82
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-06-05 21:06:31 +0900

    [Silver I] Title: 스타트와 링크, Time: 388 ms, Memory: 16060 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static boolean[] check;
+    static int[][] board;
+    static StringTokenizer st;
+    static int MIN_RESULT = Integer.MAX_VALUE;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        check = new boolean[N + 1];
+        board = new int[N + 1][N + 1];
+
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        solution(0, 1);
+        br.close();
+        bw.write(MIN_RESULT + "");
+        bw.flush();
+        bw.close();
+
+    }
+
+    public static void solution(int dep, int start) {
+        if(dep == N/2) {
+            MIN_RESULT = Math.min(MIN_RESULT, getResult());
+            return;
+        }
+
+        for(int i=start; i<=N; i++) {
+
+            check[i] = true;
+            solution(dep+1, i+1);
+            check[i] = false;
+        }
+    }
+
+    static int getResult() {
+        int start = 0;
+        int link = 0;
+
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=N; j++) {
+                if(i == j) continue;
+
+                if(check[i] && check[j]) start += board[i][j];
+                if(!check[i] && !check[j]) link += board[i][j];
+            }
+        }
+        return Math.abs(start - link);
+    }
+
+}
