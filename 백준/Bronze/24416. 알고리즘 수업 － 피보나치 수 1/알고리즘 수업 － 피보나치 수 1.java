6f222f4ac96322ac77fe46e914d586005491e356
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-06-09 10:14:45 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 504 ms, Memory: 16056 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class Main {
+    static int code1Cnt, code2Cnt;
+    static int[] f;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        f = new int[n];
+        code1Cnt = 0;
+        code2Cnt = 0;
+
+        br.close();
+        fib(n);
+        fibonacci(n);
+        bw.write(code1Cnt + " " + code2Cnt);
+        bw.flush();
+        bw.close();
+    }
+
+    public static int fib(int n) {
+        if (n == 1 || n == 2) {
+            code1Cnt++;
+            return 1;
+        } else {
+            return (fib(n - 1) + fib(n - 2));
+        }
+    }
+
+    public static int fibonacci(int n) {
+        f[0] = 1;
+        f[1] = 1;
+        for (int i = 2; i < n; i++) {
+            code2Cnt++;
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f[n - 1];
+    }
+}
