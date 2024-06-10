bd37ed0d35390b5c7997add9b83aec2c021e08b6
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-06-10 20:45:44 +0900

    [Gold III] Title: 나머지 합, Time: 528 ms, Memory: 120840 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] arr = new long[N + 1];
+        long result = 0;
+        long[] cnt = new long[M];
+        arr[0] = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= N; i++) {
+            arr[i] = (arr[i - 1] + Integer.parseInt(st.nextToken())) % M;
+            if(arr[i] == 0) result++;
+            cnt[(int) arr[i]]++;
+        }
+
+        for (int i = 0; i < M; i++) {
+            if(cnt[i] > 1) result += (cnt[i] * (cnt[i] - 1) / 2);
+        }
+
+        br.close();
+        bw.write(result + "");
+        bw.flush();
+        bw.close();
+    }
+}
