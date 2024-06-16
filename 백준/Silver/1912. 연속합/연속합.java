b7e43a6affdae4f5986652554f15879439a11351
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-06-16 19:27:40 +0900

    [Silver II] Title: 연속합, Time: 632 ms, Memory: 40312 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr;
+    static Integer[] result;
+    static int max;
+
+    static int cul(int N) {
+        if (result[N] == null) {
+            result[N] = Math.max(cul(N - 1) + arr[N], arr[N]);
+            max = Math.max(result[N], max);
+        }
+
+        return result[N];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        result = new Integer[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        result[0] = arr[0];
+        max = arr[0];
+        cul(N - 1);
+        br.close();
+        bw.write(max + "");
+        bw.flush();
+        bw.close();
+    }
+}
