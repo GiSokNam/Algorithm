6145b1254a33bc52ef4404bfba409a513e90842e
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-06-17 20:43:41 +0900

    [Gold IV] Title: 스도쿠, Time: 480 ms, Memory: 26896 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[][] arr = new int[9][9];
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        br.close();
+        sudoku(0, 0);
+
+    }
+
+    public static void sudoku(int row, int col) throws IOException {
+
+        if (col == 9) {
+            sudoku(row + 1, 0);
+            return;
+        }
+
+        if (row == 9) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    bw.write(arr[i][j] + " ");
+                }
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+            System.exit(0);
+        }
+
+        if (arr[row][col] == 0) {
+            for (int i = 1; i <= 9; i++) {
+
+                if (checkSameNum(row, col, i)) {
+                    arr[row][col] = i;
+                    sudoku(row, col + 1);
+                }
+            }
+            arr[row][col] = 0;
+            return;
+        }
+
+        sudoku(row, col + 1);
+
+    }
+
+
+    public static boolean checkSameNum(int row, int col, int value) {
+
+        for (int i = 0; i < 9; i++) {
+            if (arr[row][i] == value) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (arr[i][col] == value) {
+                return false;
+            }
+        }
+
+
+        int set_row = (row / 3) * 3;
+        int set_col = (col / 3) * 3;
+
+        for (int i = set_row; i < set_row + 3; i++) {
+            for (int j = set_col; j < set_col + 3; j++) {
+                if (arr[i][j] == value) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
