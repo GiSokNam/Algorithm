e67270f9549e2b6fa2b80dd951a72a8d87f58c00
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-07-04 19:54:01 +0900

    [Silver II] Title: 나무 자르기, Time: 540 ms, Memory: 119312 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] trees;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        trees = new int[N];
+        long max = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, trees[i]);
+        }
+
+        long result = binarySearch(0, max);
+        br.close();
+        bw.write(result + "");
+        bw.flush();
+        bw.close();
+    }
+
+    static long binarySearch(long min, long max) {
+        if (min > max) {
+            return max;
+        }
+
+        long mid = (min + max) / 2;
+        long sum = cutting(mid);
+
+        if (sum < M) {
+            return binarySearch(min, mid - 1);
+        } else {
+            return binarySearch(mid + 1, max);
+        }
+    }
+
+    static long cutting(long mid) {
+        long sum = 0;
+        for (int tree : trees) {
+            if (tree > mid) {
+                sum += (tree - mid);
+            }
+        }
+        return sum;
+    }
+}
