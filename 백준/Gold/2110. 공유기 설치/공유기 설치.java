560a064132b8e019c34f83db719ae99525cb745a
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-07-05 00:39:56 +0900

    [Gold IV] Title: 공유기 설치, Time: 324 ms, Memory: 29328 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] house;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        house = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            house[i] = Integer.parseInt(br.readLine());
+        }
+
+        br.close();
+        Arrays.sort(house);
+
+        int min = 1;
+        int max = house[N - 1] - house[0] + 1;
+
+        while (min < max) {
+            int mid = (min + max) / 2;
+
+            if (installWifi(mid) < M) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        int result = min - 1;
+        bw.write(result + "");
+        bw.flush();
+        bw.close();
+    }
+
+    static int installWifi(int distance) {
+        int count = 1;
+        int lastLocate = house[0];
+
+        for (int i = 1; i < house.length; i++) {
+            int locate = house[i];
+
+            if (locate - lastLocate >= distance) {
+                count++;
+                lastLocate = locate;
+            }
+        }
+        return count;
+    }
+}
