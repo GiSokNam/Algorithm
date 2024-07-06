75981c6ca062dc46f0dadb386e3c045c50c9c7e0
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-07-06 20:28:06 +0900

    [Gold I] Title: K번째 수, Time: 168 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        br.close();
+
+        long min = 1;
+        long max = K;
+
+        while (min < max) {
+            long mid = (min + max) / 2;
+            long sum = 0;
+
+            for (int i = 1; i <= N; i++) {
+                sum += Math.min(mid / i, N);
+            }
+
+            if (K <= sum) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        bw.write(min + "");
+        bw.flush();
+        bw.close();
+    }
+
+}
