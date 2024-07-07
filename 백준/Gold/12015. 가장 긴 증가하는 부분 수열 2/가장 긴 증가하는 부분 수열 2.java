37c2024adfbbb8ee107d462dfaf3a1a2b5be0f8b
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-07-07 09:56:55 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 664 ms, Memory: 120868 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] seq = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        ArrayList<Integer> LIS = new ArrayList<>();
+        LIS.add(seq[0]);
+        for (int i = 1; i < N; i++) {
+            int key = seq[i];
+
+            if (LIS.get(LIS.size() - 1) < key) {
+                LIS.add(key);
+            } else {
+                int min = 0;
+                int max = LIS.size() - 1;
+
+                while (min < max) {
+                    int mid = (min + max) / 2;
+                    if (LIS.get(mid) >= key) {
+                        max = mid;
+                    } else {
+                        min = mid + 1;
+                    }
+                }
+                LIS.set(max, key);
+            }
+        }
+        
+        bw.write(LIS.size() + "");
+        bw.flush();
+        bw.close();
+    }
+
+}
