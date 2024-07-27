bf35a9414f39a9d85a65b2b149e554fcbcbff9af
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-07-27 09:59:47 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,19 +1,18 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
         br.close();
         for (int i = 0; i < t; i++) {
             for (int j = 0; j <= i; j++) {
-                bw.write("*");
+                sb.append("*");
             }
-            bw.write("\n");
+            sb.append("\n");
         }
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
