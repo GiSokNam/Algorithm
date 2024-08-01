dc27f943cb1b58a20e77db3ac605ca300f843d2f
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-08-02 00:03:57 +0900

    [Bronze V] Title: 문자와 문자열, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -3,13 +3,10 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String S = br.readLine();
         int i = Integer.parseInt(br.readLine()) -1;
         br.close();
         char c = S.charAt(i);
-        bw.write(c);
-        bw.flush();
-        bw.close();
+        System.out.print(c);
     }
 }
