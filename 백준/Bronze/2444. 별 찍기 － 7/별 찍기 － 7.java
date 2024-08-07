219e6047a4f8904af7d3aa9ccc93d4cc9256fc1d
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-08-08 00:04:24 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -3,28 +3,30 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
         String star = "*";
+
         for (int i = 1; i <= N; i++) {
-            for (int j = 0; j < N-i; j++) {
-                bw.write(" ");
+            for (int j = 0; j < N - i; j++) {
+                sb.append(" ");
             }
-            for (int j = 0; j < i*2-1; j++) {
-                bw.write(star);
+            for (int j = 0; j < i * 2 - 1; j++) {
+                sb.append(star);
             }
-            bw.newLine();
+            sb.append("\n");
         }
-        for (int i = N-1; i >= 0; i--) {
-            for (int j = 0; j < N-i; j++) {
-                bw.write(" ");
+
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = 0; j < N - i; j++) {
+                sb.append(" ");
             }
-            for (int j = 0; j < i*2-1; j++) {
-                bw.write(star);
+            for (int j = 0; j < i * 2 - 1; j++) {
+                sb.append(star);
             }
-            bw.newLine();
+            sb.append("\n");
         }
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
