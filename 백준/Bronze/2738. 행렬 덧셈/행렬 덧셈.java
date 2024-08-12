42308e8c88e90003131bf9ce4ecf9b02a6ed4c8a
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-08-13 00:02:53 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 184 ms, Memory: 17872 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
@@ -22,15 +22,14 @@ public class Main {
             }
         }
         br.close();
-        
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 C[i][j] = A[i][j] + B[i][j];
-                bw.write(C[i][j] + " ");
+                sb.append(C[i][j] + " ");
             }
-            bw.newLine();
+            sb.append("\n");
         }
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
