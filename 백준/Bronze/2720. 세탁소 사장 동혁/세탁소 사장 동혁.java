dc062d4fe0f1fcddfceb58b6f5b2ba66f6590755
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-08-19 00:02:22 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 120 ms, Memory: 14628 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -3,41 +3,40 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int Q = 25;
         int D = 10;
         int N = 5;
         int P = 1;
         int T = Integer.parseInt(br.readLine());
         int C;
-        int quotient = 0;
+        int quotient;
         int remainder = 0;
         for (int i = 0; i < T; i++) {
             C = Integer.parseInt(br.readLine());
             if (C / Q >= 0) {
                 quotient = C / Q;
                 remainder = C % Q;
-                bw.write(quotient + " ");
+                sb.append(quotient + " ");
             }
             if (remainder / D >= 0) {
                 quotient = remainder / D;
                 remainder %= D;
-                bw.write(quotient + " ");
+                sb.append(quotient + " ");
             }
             if (remainder / N >= 0) {
                 quotient = remainder / N;
                 remainder %= N;
-                bw.write(quotient + " ");
+                sb.append(quotient + " ");
             }
             if (remainder / P >= 0) {
                 quotient = remainder / P;
                 remainder %= P;
-                bw.write(quotient + " ");
+                sb.append(quotient + " ");
             }
-            bw.newLine();
+            sb.append("\n");
         }
         br.close();
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
