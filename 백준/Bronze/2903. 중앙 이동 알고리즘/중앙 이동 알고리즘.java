40d4bde36d92fe3328bebf61da1b04e932cc6d92
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-08-20 07:15:16 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -3,13 +3,12 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
         br.close();
         int spot = (int) Math.pow(Math.pow(2, N) + 1, 2);
-        bw.write(spot + "");
-        bw.flush();
-        bw.close();
+        sb.append(spot);
+        System.out.print(sb);
     }
 }
