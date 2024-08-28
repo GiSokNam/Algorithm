3300d7bdc65e2a7be6a7ad6d6d6fc0a6af228453
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-08-29 00:08:56 +0900

    [Bronze I] Title: 소인수분해, Time: 116 ms, Memory: 15900 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -3,22 +3,20 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
         br.close();
 
         for (int i = 2; i <= Math.sqrt(N); i++) {
             while (N % i == 0) {
-                bw.write(i + "");
-                bw.newLine();
+                sb.append(i + "\n");
                 N /= i;
             }
         }
         if (N != 1) {
-            bw.write(N +"");
+            sb.append(N);
         }
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
