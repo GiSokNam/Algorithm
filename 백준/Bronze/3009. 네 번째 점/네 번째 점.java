4a67faf05d85d6dce7b6cfbc77bfe3dc34d14ad4
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-09-02 00:01:36 +0900

    [Bronze III] Title: 네 번째 점, Time: 116 ms, Memory: 15944 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -4,7 +4,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
@@ -34,8 +33,6 @@ public class Main {
             y = B;
         }
 
-        bw.write(x + " " +y);
-        bw.flush();
-        bw.close();
+        System.out.print(x + " " + y);
     }
 }
