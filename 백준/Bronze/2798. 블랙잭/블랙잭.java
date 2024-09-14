3cbc9f5756157ba9a918dbde09280b8dfc850b03
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-09-14 20:36:18 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
@@ -32,9 +32,7 @@ public class Main {
                 }
             }
         }
-
-        bw.write(temp + "");
-        bw.flush();
-        bw.close();
+        sb.append(temp);
+        System.out.print(sb);
     }
 }
