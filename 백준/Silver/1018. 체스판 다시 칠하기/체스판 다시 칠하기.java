b8e1e15e4a49f7210da1ddb06928e6b6d3eb8046
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-09-17 09:20:53 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
@@ -24,9 +24,8 @@ public class Main {
             }
         }
         br.close();
-        bw.write(sol + "");
-        bw.flush();
-        bw.close();
+        sb.append(sol);
+        System.out.print(sb);
     }
 
     private static int getSolution(int startRow, int startCol, String[] chessBoard) {
