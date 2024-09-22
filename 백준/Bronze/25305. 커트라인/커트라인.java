795768cd9923b342d30ec02eea073f5bcfa3607b
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-09-22 09:10:23 +0900

    [Bronze II] Title: 커트라인, Time: 108 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
@@ -18,8 +18,7 @@ public class Main {
         }
         br.close();
         Arrays.sort(x);
-        bw.write(x[N-k]+"");
-        bw.flush();
-        bw.close();
+        sb.append(x[N - k]);
+        System.out.print(sb);
     }
 }
