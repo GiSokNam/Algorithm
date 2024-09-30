86b5486188b1911538d23fb53811b713a9d812d7
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-10-01 00:14:11 +0900

    [Silver V] Title: 숫자 카드, Time: 848 ms, Memory: 143172 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         HashMap<Integer, Integer> input = new HashMap<>();
 
@@ -19,15 +19,13 @@ public class Main {
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < M; i++) {
             if (input.get(Integer.parseInt(st.nextToken())) != null) {
-                bw.write("1 ");
+                sb.append("1 ");
             } else {
-                bw.write("0 ");
+                sb.append("0 ");
             }
-
         }
         br.close();
-        bw.flush();
-        bw.close();
-
+        System.out.print(sb);
+        
     }
 }
