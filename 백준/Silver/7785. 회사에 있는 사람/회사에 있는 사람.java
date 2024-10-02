6dbf9863e8256669351cd49f0a14d95340e7fcff
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-10-03 00:07:26 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 688 ms, Memory: 56116 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         HashMap<String, String> input = new HashMap<>();
 
@@ -28,10 +28,8 @@ public class Main {
         Collections.sort(list, Collections.reverseOrder());
 
         for (String str : list) {
-            bw.write(str);
-            bw.newLine();
+            sb.append(str + "\n");
         }
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
