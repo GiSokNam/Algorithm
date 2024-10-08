a217309dd5df260ac514a6f5d1ce8d1c68109ff0
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-10-08 10:53:41 +0900

    [Bronze I] Title: 최소공배수, Time: 132 ms, Memory: 16316 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
@@ -16,12 +16,10 @@ public class Main {
             B = Integer.parseInt(st.nextToken());
             int gcd = GCD(A, B);
             int result = A * B / gcd;
-            bw.write(result + "");
-            bw.newLine();
+            sb.append(result + "\n");
         }
         br.close();
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 
     public static int GCD(int a, int b) {
