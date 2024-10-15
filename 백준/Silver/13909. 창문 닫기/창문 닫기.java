3f21d279925798554f5dede7a7c2f5c21c2c0051
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-10-16 00:16:59 +0900

    [Silver V] Title: 창문 닫기, Time: 96 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.java
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -3,18 +3,17 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringBuilder sb = new StringBuilder();
+        
         int N = Integer.parseInt(br.readLine());
         int cnt = 0;
-        
+
         for (int i = 1; i * i <= N; i++) {
             cnt++;
         }
-
-        bw.write(cnt + "");
         br.close();
-        bw.flush();
-        bw.close();
+
+        sb.append(cnt);
+        System.out.print(sb);
     }
 }
