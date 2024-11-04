ae2c69b7525e35700cb15da5d89b9b21c1820b98
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-11-05 00:04:19 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 728 ms, Memory: 42160 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
@@ -35,10 +35,8 @@ public class Main {
         });
 
         for (String word : words) {
-            bw.write(word);
-            bw.newLine();
+            sb.append(word + "\n");
         }
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
