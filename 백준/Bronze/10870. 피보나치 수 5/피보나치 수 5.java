16f1d93f1184a08db38404b10e120e939cf6a1c2
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-11-07 00:38:15 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         ArrayList<Integer> list = new ArrayList<>();
@@ -16,8 +15,6 @@ public class Main {
         }
 
         br.close();
-        bw.write(list.get(N) + "");
-        bw.flush();
-        bw.close();
+        System.out.print(list.get(N));
     }
 }
