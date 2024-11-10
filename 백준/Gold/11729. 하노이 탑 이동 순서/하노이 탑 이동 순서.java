ea6e78ee14d1ab8d9c4ffd5347172865c12de913
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-11-11 00:05:37 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 460 ms, Memory: 71444 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -2,26 +2,24 @@ import java.io.*;
 
 public class Main {
 
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int cnt = (int) Math.pow(2, N) - 1;
-        bw.write(cnt + "");
-        bw.newLine();
+        sb.append(cnt + "\n");
         move(N, 1, 3, 2);
         br.close();
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 
     static void move(int N, int from, int to, int temp) throws IOException {
         if (N == 1) {
-            bw.write(from + " " + to + "\n");
+            sb.append(from + " " + to + "\n");
         } else {
             move(N - 1, from, temp, to);
-            bw.write(from + " " + to + "\n");
+            sb.append(from + " " + to + "\n");
             move(N - 1, temp, to, from);
         }
     }
