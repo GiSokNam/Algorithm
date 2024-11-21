c369187b34e69b533e8226eb833af8618f4a39f1
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-11-21 09:59:10 +0900

    [Silver II] Title: 연속합, Time: 272 ms, Memory: 40296 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -18,7 +18,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         arr = new int[N];
         result = new Integer[N];
@@ -32,8 +32,7 @@ public class Main {
         max = arr[0];
         cul(N - 1);
         br.close();
-        bw.write(max + "");
-        bw.flush();
-        bw.close();
+        sb.append(max);
+        System.out.print(sb);
     }
 }
