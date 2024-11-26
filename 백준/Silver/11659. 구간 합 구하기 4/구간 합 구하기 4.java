c31ca5f1c62e308bfd46e76f3f043272a6b106fc
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-11-27 00:05:17 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 620 ms, Memory: 62224 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
@@ -22,11 +22,9 @@ public class Main {
             int I = Integer.parseInt(st.nextToken());
             int J = Integer.parseInt(st.nextToken());
             int sum = arr[J] - arr[I - 1];
-            bw.write( sum+ "");
-            bw.newLine();
+            sb.append(sum + "\n");
         }
         br.close();
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
