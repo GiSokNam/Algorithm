e640be34291834abd7260e9f59acac75cf6b229a
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-11-28 00:20:13 +0900

    [Silver III] Title: 수열, Time: 240 ms, Memory: 24080 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
@@ -21,8 +21,7 @@ public class Main {
             result = Math.max(result, arr[i] - arr[i - K]);
         }
         br.close();
-        bw.write(result + "");
-        bw.flush();
-        bw.close();
+        sb.append(result);
+        System.out.print(sb);
     }
 }
