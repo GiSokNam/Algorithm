d1b159f810f9c5ec58e7c3240d6d9365880ab127
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-12-03 10:18:02 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,10 +1,10 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
@@ -27,8 +27,7 @@ public class Main{
             }
         }
 
-        bw.write(cnt + "");
-        bw.flush();
-        bw.close();
+        sb.append(cnt);
+        System.out.print(sb);
     }
 }
