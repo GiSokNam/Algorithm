2e87317cde29092cb45873f9c1e9e7227e74957c
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-12-05 00:01:46 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
         int[] time = new int[N];
@@ -25,8 +25,7 @@ public class Main {
             prev += time[i];
         }
 
-        bw.write(sum + "");
-        bw.flush();
-        bw.close();
+        sb.append(sum);
+        System.out.print(sb);
     }
 }
