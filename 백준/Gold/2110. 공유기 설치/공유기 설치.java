01ef271d403e1bed880e3d8335727c314fc32b05
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-12-12 00:13:58 +0900

    [Gold IV] Title: 공유기 설치, Time: 268 ms, Memory: 29016 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         N = Integer.parseInt(st.nextToken());
@@ -37,9 +37,8 @@ public class Main {
         }
 
         int result = min - 1;
-        bw.write(result + "");
-        bw.flush();
-        bw.close();
+        sb.append(result);
+        System.out.print(sb);
     }
 
     static int installWifi(int distance) {
