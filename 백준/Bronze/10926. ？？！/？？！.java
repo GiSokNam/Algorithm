62ee34d6d23c5ac4015e1aa34192e27ce42ef868
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2024-12-29 00:02:19 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String a = br.readLine();
-        System.out.println(a+"??!");
+        System.out.print(a+"??!");
     }
 }
