8fa38a38292a198808211fa315d4f443fce8fbc8
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2025-01-07 10:48:44 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,23 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         Long a = Long.parseLong(br.readLine());
 
         if (a >= 90) {
-            System.out.println("A");
+            sb.append("A");
         } else if (a >= 80) {
-            System.out.println("B");
+            sb.append("B");
         } else if (a >= 70) {
-            System.out.println("C");
+            sb.append("C");
         } else if (a >= 60) {
-            System.out.println("D");
+            sb.append("D");
         } else {
-            System.out.println("F");
+            sb.append("F");
         }
+        System.out.print(sb);
     }
 }
