be84c70486285f6eb42af4189d295e37c6c475a7
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2025-01-08 00:03:01 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -5,8 +5,12 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         Long a = Long.parseLong(br.readLine());
-        if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)) System.out.println("1");
-        else System.out.println("0");
+        br.close();
+
+        if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)) sb.append("1");
+        else sb.append("0");
+        System.out.print(sb);
     }
 }
