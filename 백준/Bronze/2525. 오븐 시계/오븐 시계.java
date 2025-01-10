88f45ee14d7c402ab0b2512ccf6433df0c1acbf9
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2025-01-11 01:07:04 +0900

    [Bronze III] Title: 오븐 시계, Time: 124 ms, Memory: 16044 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,19 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.StringTokenizer;
- 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int h = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int mm = Integer.parseInt(br.readLine());
-        int min = 60 * h + m;  
-        min += mm;
-        int hour = (min / 60) % 24;
-        int minute = min % 60;
-        System.out.println(hour + " " + minute);
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(br.readLine());
+        br.close();
+        int min = (H * 60) + M + C;
+        int h = (min / 60) % 24;
+        int m = min % 60;
+
+        sb.append(h + " " + m);
+        System.out.println(sb);
     }
 }
