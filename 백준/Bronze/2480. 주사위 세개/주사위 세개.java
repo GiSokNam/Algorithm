80ad1a02f20bd8384f7096f0d2fdd07f49762351
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2025-01-12 00:58:32 +0900

    [Bronze IV] Title: 주사위 세개, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -13,15 +13,15 @@ public class Main {
         br.close();
 	
 	    if (a == b && a == c) {
-                System.out.println(10000 + a * 1000);
+                System.out.print(10000 + a * 1000);
         } else if (a == b) {
-            System.out.println(1000 + a * 100);
+            System.out.print(1000 + a * 100);
         } else if (a == c) {
-            System.out.println(1000 + a * 100);
+            System.out.print(1000 + a * 100);
         } else if (b == c) {
-            System.out.println(1000 + b * 100);
+            System.out.print(1000 + b * 100);
         } else {
-            System.out.println(Math.max(a, Math.max(b,c)) * 100);
+            System.out.print(Math.max(a, Math.max(b,c)) * 100);
         }
          
     }
