7713c77294e48f3d2f92bd9bc503d1a4d16d65af
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2025-01-13 09:35:12 +0900

    [Bronze V] Title: 구구단, Time: 128 ms, Memory: 16080 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,17 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         br.close();
 
         for (int i = 1; i < 10; i++) {
-            System.out.println(n + " * " + i + " = " + (n*i));
+            sb.append(n + " * " + i + " = " + (n * i) + "\n");
         }
-
+        System.out.println(sb);
     }
 }
