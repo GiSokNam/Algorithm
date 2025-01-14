19a89f224158dbd5ae2af6395676b4e3bb25f992
GiSokNam <110178774+GiSokNam@users.noreply.github.com>
2025-01-14 13:48:13 +0900

    [Bronze V] Title: A+B - 3, Time: 124 ms, Memory: 15892 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -7,18 +7,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
         int[] sum = new int[t];
         for (int i = 0; i < t; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            sum[i] = a+b;
+            sum[i] = a + b;
         }
 
-        for (int i = 0; i < t; i++) {
-            System.out.println(sum[i]);
+        for (int i : sum) {
+            sb.append(i + "\n");
         }
-
+        System.out.println(sb);
     }
 }
