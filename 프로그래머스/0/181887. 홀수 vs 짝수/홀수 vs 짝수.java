d0ea83b0a4a12f2ffd30d3a53ced8d9365d32a23
GiSeokNam <110178774+GiSokNam@users.noreply.github.com>
2025-05-02 21:14:10 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 5.90 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,17 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int oddSum = IntStream.range(0, num_list.length)
+                              .filter(i -> i % 2 == 0)
+                              .map(i -> num_list[i])
+                              .sum();
+
+        int evenSum = IntStream.range(0, num_list.length)
+                               .filter(i -> i % 2 != 0)
+                               .map(i -> num_list[i])
+                               .sum();
+
+        return Math.max(oddSum, evenSum);
+    }
+}
