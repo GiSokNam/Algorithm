563f16f8f172aeff1e745c741bf36448d6a14471
GiSeokNam <110178774+GiSokNam@users.noreply.github.com>
2025-09-10 19:41:00 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b) {
+        int g = gcd(a, b);
+        b /= g;
+        while (b % 2 == 0) b /= 2;
+        while (b % 5 == 0) b /= 5;
+        return b == 1 ? 1 : 2;
+    }
+    private int gcd(int x, int y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
+}
