3b600d95dc66d7c98551d63c3a5bb0affff124de
GiSeokNam <110178774+GiSokNam@users.noreply.github.com>
2026-01-06 19:06:40 +0900

    [level 1] Title: 기사단원의 무기, Time: 15.68 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int[] divisorCount = new int[number + 1];
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = i; j <= number; j += i) {
+                divisorCount[j]++;
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += divisorCount[i] > limit ? power : divisorCount[i];
+        }
+
+        return answer;
+    }
+}
