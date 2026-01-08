221db040d1f284fc7f3e722b0479188a3930fa08
GiSeokNam <110178774+GiSokNam@users.noreply.github.com>
2026-01-08 22:58:11 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 16.53 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder left = new StringBuilder();
+
+        for (int i = 1; i < food.length; i++) {
+            left.append(String.valueOf(i).repeat(food[i] / 2));
+        }
+
+        String right = new StringBuilder(left).reverse().toString();
+        return left + "0" + right;
+    }
+}
