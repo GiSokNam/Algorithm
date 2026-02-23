05aed61c43f72a1124d59dcb3006577cb2b76613
GiSeokNam <110178774+GiSokNam@users.noreply.github.com>
2026-02-23 19:02:21 +0900

    [level 0] Title: qr code, Time: 0.12 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,13 +1,13 @@
 class Solution {
     public String solution(int q, int r, String code) {
         StringBuilder result = new StringBuilder();
-        
-        for (int i = 0; i < code.length(); i++) {
-            if (i % q == r) {
-                result.append(code.charAt(i));
+
+        for (int index = 0; index < code.length(); index++) {
+            if (index % q == r) {
+                result.append(code.charAt(index));
             }
         }
-        
+
         return result.toString();
     }
 }
