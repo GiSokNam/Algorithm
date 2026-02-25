aa7004f280c4ed132608099cc039a8dcdaeb94d1
GiSeokNam <110178774+GiSokNam@users.noreply.github.com>
2026-02-25 19:37:51 +0900

    [level 0] Title: 배열 만들기 1, Time: 5.29 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,11 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int n, int k) {
-        int[] answer = new int[n / k];
-        
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = k * (i + 1);
-        }
-        
-        return answer;
+        return IntStream
+                .iterate(k, i -> i <= n, i -> i + k)
+                .toArray();
     }
 }
